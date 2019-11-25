1295c7992ab133565790e35d32be6af6a3720179
Forrest Waters <fwaters@anaconda.com>
2019-11-25 07:26:19 -0600

    bump gdal ver and skip additional test that fails intermittently on CI due to file locking

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,9 +1,14 @@
 :: There is no `gdal-config` on Windows so we need to hardcode gdal's version.
-set GDAL_VERSION=3.0.1
+set GDAL_VERSION=3.0.2
+REM For some reason pip is failing
+REM See https://circleci.com/gh/conda-forge/rasterio-feedstock/474?utm_campaign=vcs-integration-link&utm_medium=referral&utm_source=github-build-link
+:: "%PYTHON%" -m pip install --no-deps --ignore-installed . ^
+::                           --global-option=build_ext ^
+::                           --global-option="-I%LIBRARY_INC%" ^
+::                           --global-option="-L%LIBRARY_LIB%" ^
+::                           --global-option="-lgdal_i"
+:: if errorlevel 1 exit 1
 
-"%PYTHON%" -m pip install --no-deps --ignore-installed . ^
-                          --global-option=build_ext ^
-                          --global-option="-I%LIBRARY_INC%" ^
-                          --global-option="-L%LIBRARY_LIB%" ^
-                          --global-option="-lgdal_i"
+
+"%PYTHON%" setup.py build_ext -I"%LIBRARY_INC%" -lgdal_i -L"%LIBRARY_LIB%" install --single-version-externally-managed --record record.txt
 if errorlevel 1 exit 1
