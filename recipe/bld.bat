6003576c80b44883fd610dc034625babc15cbdc1
anaconda-pkg-build <66446536+anaconda-pkg-build@users.noreply.github.com>
2022-04-22 16:19:15 +0200

    rasterio 1.2.10 (#4)
    * DOR update to 1.2.10
    
    * Update dependencies
    
    * Update build.sh
    
    * Fix a test command
    
    * Add doc url

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,14 +1,9 @@
-:: There is no `gdal-config` on Windows so we need to hardcode gdal's version.
-set GDAL_VERSION=3.0.2
-REM For some reason pip is failing
-REM See https://circleci.com/gh/conda-forge/rasterio-feedstock/474?utm_campaign=vcs-integration-link&utm_medium=referral&utm_source=github-build-link
-:: "%PYTHON%" -m pip install --no-deps --ignore-installed . ^
-::                           --global-option=build_ext ^
-::                           --global-option="-I%LIBRARY_INC%" ^
-::                           --global-option="-L%LIBRARY_LIB%" ^
-::                           --global-option="-lgdal_i"
-:: if errorlevel 1 exit 1
+:: There is no `gdal-config` on Windows so we need figure it out from gdalinfo
+for /F "USEBACKQ tokens=2 delims=, " %%F in (`gdalinfo --version`) do (
+set GDAL_VERSION=%%F
+)
+if errorlevel 1 exit 1
+echo "set GDAL_VERSION=%GDAL_VERSION%"
 
-
-"%PYTHON%" setup.py build_ext -I"%LIBRARY_INC%" -lgdal_i -L"%LIBRARY_LIB%" install --single-version-externally-managed --record record.txt
+%PYTHON% setup.py build_ext -I"%LIBRARY_INC%" -lgdal_i -L"%LIBRARY_LIB%" install --single-version-externally-managed --record record.txt
 if errorlevel 1 exit 1
